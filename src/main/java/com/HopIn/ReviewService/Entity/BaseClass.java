df6bfbcd8941601d0357d9c9640ad1326cb4d1d1
Mayank Gopal <mayankgopal05@gmail.com>
2025-08-21 02:28:40 +0530

    added the BookingStatus Enum

--- src/main/java/com/HopIn/ReviewService/Entity/BaseClass.java
+++ src/main/java/com/HopIn/ReviewService/Entity/BaseClass.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class BaseClass {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITYgit )
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @CreatedDate
---
+++ src/main/java/com/HopIn/ReviewService/Entity/BookingStatus.java
@@ -0,0 +1,14 @@
+package com.HopIn.ReviewService.Entity;
+
+public enum BookingStatus {
+
+    REQUESTED,
+    ACCEPTED,
+    REJECTED,
+    CANCELLED,
+    ONGOING,
+    COMPLETED,
+    PAYMENT_PENDING,
+    PAYMENT_COMPLETED
+
+}
