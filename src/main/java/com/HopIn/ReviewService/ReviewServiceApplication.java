9eceb1ff778106f413cdffc36dd99cdf64f33831
Mayank Gopal <mayankgopal05@gmail.com>
2025-08-21 00:27:39 +0530

    added Jpa auditing annotation

--- src/main/java/com/HopIn/ReviewService/ReviewServiceApplication.java
+++ src/main/java/com/HopIn/ReviewService/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package com.HopIn.ReviewService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
