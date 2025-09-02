822e004de5ba05e13e95e3693118835a06295a1e
Mayank Gopal <mayankgopal05@gmail.com>
2025-09-03 02:32:09 +0530

    added query derivatives method for Review repo

--- src/main/java/com/HopIn/ReviewService/Repositories/ReviewRepository.java
+++ src/main/java/com/HopIn/ReviewService/Repositories/ReviewRepository.java
@@ -1,10 +1,15 @@
 package com.HopIn.ReviewService.Repositories;
 
+import com.HopIn.ReviewService.Entity.Driver;
 import com.HopIn.ReviewService.Entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Long> {
-
+Optional<List<Review>> findByDriverId(Long id);
+Optional<List<Review>> findByDriverIdIn(List<Long> ids);
 }
